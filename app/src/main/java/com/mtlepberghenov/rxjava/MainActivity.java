3b5152486e393928be428a2bb73a604ef60eda57
mtlepberghenov <marat.tlepberghenov@endava.com>
2018-10-02 16:48:14 +0300

    interval

--- app/src/main/java/com/mtlepberghenov/rxjava/MainActivity.java
+++ app/src/main/java/com/mtlepberghenov/rxjava/MainActivity.java
@@ -2,6 +2,14 @@ package com.mtlepberghenov.rxjava;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+import timber.log.Timber;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+        }
+
+        Model model = new Model();
+
+        model.getData()
+                .subscribe(new Observer<Long>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                Timber.d("onSubscribe");
+            }
+
+            @Override
+            public void onNext(Long aLong) {
+                Timber.d("onNext: %s", aLong);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Timber.d("onError");
+            }
+
+            @Override
+            public void onComplete() {
+                Timber.d("onComplete");
+            }
+        });
+    }
+}
+
+class Model {
+
+    public Observable<Long> getData() {
+        return Observable.interval(500, TimeUnit.MILLISECONDS);
     }
 }
